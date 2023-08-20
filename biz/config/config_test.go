1f5fd7742f2dbfe3464acdcac9ab3a8b691068b1
liruohrh <2372221537@qq.com>
2023-08-20 12:24:21 +0800

    config的profile功能优化: 1.不用在profile的配置文件再全写一遍,在profile中的会替换默认的. 2. 除了从环境变量读取profile， 添加了从默认配置读取的方式

--- biz/config/config_test.go
+++ biz/config/config_test.go
@@ -2,13 +2,10 @@ package config
 
 import (
 	"encoding/json"
-	"os"
 	"testing"
-	"tiktok/pkg/constant"
 )
 
 func TestInitConfig(t *testing.T) {
-	os.Setenv(constant.ProfileEnv, "dev")
 	Init("../../config.ini")
 	marshal, err := json.MarshalIndent(C, "  ", "    ")
 	if err != nil {
---
+++ biz/config/logger.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm/logger"
+	"os"
+)
+
+type Logger struct {
+	LogrusLevel string
+	GormLevel   string
+	GinLevel    string
+}
+
+func (l Logger) GetGinMode() string {
+	if os.Getenv(gin.EnvGinMode) != "" {
+		return ""
+	}
+	switch l.GinLevel {
+	case "DEBUG":
+		return gin.DebugMode
+	case "RELEASE":
+		return gin.ReleaseMode
+	case "TEST":
+		return gin.TestMode
+	}
+	panic("has not GinLevel=" + l.GinLevel)
+}
+func (l Logger) GetGormLogLevel() logger.LogLevel {
+	switch l.GormLevel {
+	case "ERROR":
+		return logger.Error
+	case "WARN":
+		return logger.Warn
+	case "INFO":
+		return logger.Info
+	case "SILENT":
+		return logger.Silent
+	}
+	panic("has not GormLevel=" + l.GormLevel)
+}
+func (l Logger) GetLogrusLogLevel() logrus.Level {
+	switch l.LogrusLevel {
+	case "PANIC":
+		return logrus.PanicLevel
+	case "FATAL":
+		return logrus.FatalLevel
+	case "ERROR":
+		return logrus.ErrorLevel
+	case "WARN":
+		return logrus.WarnLevel
+	case "INFO":
+		return logrus.InfoLevel
+	case "DEBUG":
+		return logrus.DebugLevel
+	case "TRACE":
+		return logrus.TraceLevel
+	}
+	panic("has not LogrusLevel=" + l.LogrusLevel)
+}
